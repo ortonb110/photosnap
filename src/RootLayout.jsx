0560ec229ed569e24fa4d149fc728c6280a86478
Orton Bright <ortonb110@gmail.com>
2023-05-30 22:10:59 +0000

    Working on Navigation bar

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,19 +1,20 @@
 import React, { Fragment } from 'react'
 import { NavLink,  Outlet} from 'react-router-dom'
+import logo from './assets/shared/desktop/logo.svg'
 
 export default function RootLayout() {
   return (
     <Fragment>
     <header className=' px-[16.5rem] py-[2.8rem]'>
-        <nav className='flex justify-between '>
-            <NavLink to={'/'}>logo</NavLink>
+        <nav className='flex justify-between items-center'>
+            <NavLink to={'/'}><img src={logo} alt="Photosnap Logo" className='w-[17rem] '/></NavLink>
             <div className='flex justify-between w-[26.9rem]'>
             <NavLink className={`nav-items`} to={'stories'}>stories</NavLink>
             <NavLink className={`nav-items`} to={'features'}>features</NavLink>
             <NavLink className={`nav-items`} to={'pricing'}>pricing</NavLink>
             </div>
             <div>
-            <NavLink to={'/'}>get an invite</NavLink>
+            <NavLink to={'/'} className={`btn bg-black text-white btn_default`}>get an invite</NavLink>
             <button className='md:hidden'>toggle</button>
             </div>
         </nav>
