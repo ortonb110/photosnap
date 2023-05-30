25b89c7a2e2623e304a4a3ffada63aae00f1b11c
Orton Bright <ortonb110@gmail.com>
2023-05-30 23:49:52 +0000

    Hero Component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section>
-      <div>
+    <section className="w-full grid auto-cols-auto grid-flow-col h-[65rem]">
+      <div className="w-[61rem]">
         <h1>create and share your phot stories</h1>
         <p>
           Photosnap is a platform for photographers and visual storytellers. We
@@ -11,14 +11,14 @@ export default function Hero() {
         </p>
         <button>get an invite</button>
       </div>
-      <div>
-        <picture>
+      <div className="w-full">
+        <picture >
           <source
             media="(min-width: 1024px)"
             srcset={require('../assets/home/desktop/create-and-share.jpg')}
           />
           <source media="(min-width: 768px)" srcset={require('../assets/home/tablet/create-and-share.jpg')} />
-          <img
+          <img className="w-full object-cover h-[65rem]"
             src={require('../assets/home/mobile/create-and-share.jpg')}
             alt="Create and Share       "
           />
--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -8,7 +8,7 @@ import ToggleBtn from './Components/ToggleBtn'
 export default function RootLayout() {
   return (
     <Fragment>
-    <header className=' px-[2.4rem] md:px-[3.9rem] lg:px-[16.5rem] py-[2.8rem]'>
+    <header className=' px-[2.4rem] md:px-[3.9rem] lg:px-[16.5rem] py-[1.6rem]'>
         <nav className='flex justify-between items-center'>
             <NavLink to={'/'}><img src={logo} alt="Photosnap Logo" className='w-[17rem] '/></NavLink>
             <div className='md:flex justify-between w-[26.9rem] hidden items-center'>
@@ -17,7 +17,7 @@ export default function RootLayout() {
             <NavLink className={`nav-items`} to={'pricing'}>pricing</NavLink>
             </div>
             <div>
-            <NavLink to={'/'} className={`btn bg-black text-white btn_default hidden md:flex`}>get an invite</NavLink>
+            <NavLink to={'/'} className={`btn bg-black text-white btn_default hidden md:flex `}>get an invite</NavLink>
             <ToggleBtn/>
             </div>
         </nav>
