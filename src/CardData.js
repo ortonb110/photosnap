79e45806001665f8dc82a9635e743b11dceb0166
Orton Bright <ortonb110@gmail.com>
2023-06-04 14:07:37 +0000

    Added Data to Object

---
+++ src/CardData.js
@@ -0,0 +1,162 @@
+export const CardData = [
+  {
+    date: "April 16th 2020",
+    title: "the mountains",
+    author: "john appleseed",
+    image: {
+      desktop: require("./assets/stories/desktop/mountains.jpg"),
+      tablet: require("./assets/stories/tablet/mountains.jpg"),
+      mobile: require("./assets/stories/mobile/mountains.jpg"),
+    },
+  },
+  {
+    date: "April 14th 2020",
+    title: "sunset cityscapes",
+    author: "benjamin cruz",
+    image: {
+      desktop: require("./assets/stories/desktop/cityscapes.jpg"),
+      tablet: require("./assets/stories/tablet/cityscapes.jpg"),
+      mobile: require("./assets/stories/mobile/cityscapes.jpg"),
+    },
+  },
+  {
+    date: "April 11th 2020",
+    title: "18 days voyage",
+    author: "alexei borodin",
+    image: {
+      desktop: require("./assets/stories/desktop/18-days-voyage.jpg"),
+      tablet: require("./assets/stories/tablet/18-days-voyage.jpg"),
+      mobile: require("./assets/stories/mobile/18-days-voyage.jpg"),
+    },
+  },
+  {
+    date: "April 9th 2020",
+    title: "architecturals",
+    author: "samantha brooke",
+    image: {
+      desktop: require("./assets/stories/desktop/architecturals.jpg"),
+      tablet: require("./assets/stories/tablet/architecturals.jpg"),
+      mobile: require("./assets/stories/mobile/architecturals.jpg"),
+    },
+  },
+  {
+    date: "April 7th 2020",
+    title: "world tour 2019",
+    author: "timothy wagner",
+    image: {
+      desktop: require("./assets/stories/desktop/world-tour.jpg"),
+      tablet: require("./assets/stories/tablet/world-tour.jpg"),
+      mobile: require("./assets/stories/mobile/world-tour.jpg"),
+    },
+  },
+  {
+    date: "April 3rd 2020",
+    title: "unforeseen corners",
+    author: "william malcolm",
+    image: {
+      desktop: require("./assets/stories/desktop/unforeseen-corners.jpg"),
+      tablet: require("./assets/stories/tablet/unforeseen-corners.jpg"),
+      mobile: require("./assets/stories/mobile/unforeseen-corners.jpg"),
+    },
+  },
+  {
+    date: "march 29th 2020",
+    title: "king of africa: part II",
+    author: "tim hillenburg",
+    image: {
+      desktop: require("./assets/stories/desktop/king-on-africa.jpg"),
+      tablet: require("./assets/stories/tablet/king-on-africa.jpg"),
+      mobile: require("./assets/stories/mobile/king-on-africa.jpg"),
+    },
+  },
+  {
+    date: "march 21st 2020",
+    title: "the trip to nowhere",
+    author: "felicia rourke",
+    image: {
+      desktop: require("./assets/stories/desktop/trip-to-nowhere.jpg"),
+      tablet: require("./assets/stories/tablet/trip-to-nowhere.jpg"),
+      mobile: require("./assets/stories/mobile/trip-to-nowhere.jpg"),
+    },
+  },
+  {
+    date: "march 19th 2020",
+    title: "rage of the sea",
+    author: "mohammed abdul",
+    image: {
+      desktop: require("./assets/stories/desktop/rage-of-the-sea.jpg"),
+      tablet: require("./assets/stories/tablet/rage-of-the-sea.jpg"),
+      mobile: require("./assets/stories/mobile/rage-of-the-sea.jpg"),
+    },
+  },
+  {
+    date: "march 16th 2020",
+    title: "running free",
+    author: "michelle",
+    image: {
+      desktop: require("./assets/stories/desktop/running-free.jpg"),
+      tablet: require("./assets/stories/tablet/running-free.jpg"),
+      mobile: require("./assets/stories/mobile/running-free.jpg"),
+    },
+  },
+  {
+    date: "march 11th 2020",
+    title: "behind the waves",
+    author: "lamar wilson",
+    image: {
+      desktop: require("./assets/stories/desktop/behind-the-waves.jpg"),
+      tablet: require("./assets/stories/tablet/behind-the-waves.jpg"),
+      mobile: require("./assets/stories/mobile/behind-the-waves.jpg"),
+    },
+  },
+  {
+    date: "march 9th 2020",
+    title: "calm waters",
+    author: "samantha brooke",
+    image: {
+      desktop: require("./assets/stories/desktop/calm-waters.jpg"),
+      tablet: require("./assets/stories/tablet/calm-waters.jpg"),
+      mobile: require("./assets/stories/mobile/calm-waters.jpg"),
+    },
+  },
+  {
+    date: "march 5th 2020",
+    title: "the milky way",
+    author: "benjamin cruz",
+    image: {
+      desktop: require("./assets/stories/desktop/milky-way.jpg"),
+      tablet: require("./assets/stories/tablet/milky-way.jpg"),
+      mobile: require("./assets/stories/mobile/milky-way.jpg"),
+    },
+  },
+  {
+    date: "march 4th 2020",
+    title: "night at the dark forest",
+    author: "mohammed abdul",
+    image: {
+      desktop: require("./assets/stories/desktop/dark-forest.jpg"),
+      tablet: require("./assets/stories/tablet/dark-forest.jpg"),
+      mobile: require("./assets/stories/mobile/dark-forest.jpg"),
+    },
+  },
+  {
+    date: "march 1st 2020",
+    title: "somwarpet's beauty",
+    author: "michelle",
+    image: {
+      desktop: require("./assets/stories/desktop/somwarpet.jpg"),
+      tablet: require("./assets/stories/tablet/somwarpet.jpg"),
+      mobile: require("./assets/stories/mobile/somwarpet.jpg"),
+    },
+  },
+  {
+    date: "february 25th 2020",
+    title: "land of dreams",
+    author: "william malcolm",
+    image: {
+      desktop: require("./assets/stories/desktop/land-of-dreams.jpg"),
+      tablet: require("./assets/stories/tablet/land-of-dreams.jpg"),
+      mobile: require("./assets/stories/mobile/land-of-dreams.jpg"),
+    },
+  },
+];
--- src/Components/Hero-Reversed.jsx
+++ src/Components/Hero-Reversed.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import BtnArrow from "./BtnArrow";
 
-export default function HeroReversed({data}) {
-    const {header, details, image, design} =data
-  const {desktop, mobile, tablet} = image
+export default function HeroReversed({ data }) {
+  const { header, details, image, design } = data;
+  const { desktop, mobile, tablet } = image;
   return (
     <section className="grid h-[71.3rem] md:h-[65rem] md:grid-reversed">
       <div className="h-[29.4rem] md:h-fit">
         <picture>
-          <source
-            media="(min-width: 1024px)"
-            srcset={desktop}
-          />
-          <source
-            media="(min-width: 768px)"
-            srcset={tablet}
-          />
+          <source media="(min-width: 1024px)" srcset={desktop} />
+          <source media="(min-width: 768px)" srcset={tablet} />
           <img
             className=" w-full object-cover h-[29.4rem] md:h-[65rem] "
             src={mobile}
@@ -23,14 +17,14 @@ export default function HeroReversed({data}) {
           />
         </picture>
       </div>
-      <div className={` w-full  ${design === 'reverse'? 'bg-white text-black' : 'bg-black text-white'} flex justify-center items-center  md:order-none h-[41.9rem] md:h-full`}>
+      <div
+        className={` w-full  ${
+          design === "reverse" ? "bg-white text-black" : "bg-black text-white"
+        } flex justify-center items-center  md:order-none h-[41.9rem] md:h-full`}
+      >
         <div className="md:h-[30.4rem] px-[3.3rem] py-[7.2rem] md:py-0 md:px-[11.2rem] hero_pad">
-          <h1 className="uppercase font-[600] mb-[2rem]">
-            {header}
-          </h1>
-          <p className="mb-[4rem] text-gray-400">
-            {details}
-          </p>
+          <h1 className="uppercase font-[600] mb-[2rem]">{header}</h1>
+          <p className="mb-[4rem] text-gray-400">{details}</p>
           <BtnArrow stroke={design} />
         </div>
       </div>
