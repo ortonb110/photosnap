472b9df70c8ef2cd1f98e2a8728633f59e547866
Orton Bright <ortonb110@gmail.com>
2023-06-05 23:55:12 +0000

    Fixing Footer responsive issues

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import FooterLinks from "./FooterLinks"
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white w-full">
+    <footer className="bg-black text-white w-full px-[17.1rem] py-[6.4rem]">
         <FooterLinks/>
     </footer>
   )
--- src/Components/FooterLinks.jsx
+++ src/Components/FooterLinks.jsx
@@ -1,18 +1,53 @@
-import { NavLink } from "react-router-dom"
-import logo from '../assets/shared/desktop/logo-white.svg'
+import { NavLink } from "react-router-dom";
+import logo from "../assets/shared/desktop/logo-white.svg";
+import facebook from "../assets/shared/desktop/facebook.svg";
+import youtube from "../assets/shared/desktop/youtube.svg";
+import twitter from "../assets/shared/desktop/twitter.svg";
+import pinterest from "../assets/shared/desktop/pinterest.svg";
+import instagram from "../assets/shared/desktop/instagram.svg";
+
 export default function FooterLinks() {
   return (
-    <div>
-        <NavLink to={'/'}> <img src={logo} alt="Photosnap Logo" className=''/> </NavLink>
-        <div>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'stories'}>Stories</NavLink>
-            <NavLink to={'features'}>features</NavLink>
-            <NavLink to={'pricing'}>pricing</NavLink>
-        </div>
-        <div>
-            
-        </div>
+    <div className="grid grid-rows-3 grid-flow-col w-[35.1rem] items-center relative h-[12.2rem]">
+      <NavLink to={"/"} className={`col-span-2`}>
+        {" "}
+        <img src={logo} alt="Photosnap Logo" className="w-[17rem]" />{" "}
+      </NavLink>
+      <div className="flex flex-col row-span-3 uppercase gap-2 font-bold tracking-[2px]">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"stories"}>Stories</NavLink>
+        <NavLink to={"features"}>features</NavLink>
+        <NavLink to={"pricing"}>pricing</NavLink>
+      </div>
+      <div className="col-span-end absolute bottom-0">
+        <ul className="flex gap-[1.3rem]">
+          <li>
+            <a href="#">
+              <img src={facebook} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={youtube} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={twitter} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={pinterest} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={instagram} alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
