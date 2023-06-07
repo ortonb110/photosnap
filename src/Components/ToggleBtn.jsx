66403e04502f82f3eff50cc639a0863ed04e55b2
Orton Bright <ortonb110@gmail.com>
2023-06-07 00:19:30 +0000

    Navigation Bar Responsive layout

--- src/Components/ToggleBtn.jsx
+++ src/Components/ToggleBtn.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function ToggleBtn() {
+export default function ToggleBtn({toggleBtnHandler, open}) {
   return (
-    <button className="md:hidden flex flex-col gap-[4px]">
-      <span className="w-[2rem] h-[2px] bg-black"></span>
-      <span className="w-[2rem] h-[2px] bg-black"></span>
+    <button className="md:hidden flex flex-col gap-[4px]" onClick={toggleBtnHandler}>
+      <span className={`w-[2rem] h-[2px] bg-black transition-all ease-in-out duration-200 ${open? 'translate-x-2': "rotate-0"}`}></span>
+      <span className={`w-[2rem] h-[2px] bg-black transition-all ease-in-out duration-200 ${open? '-translate-x-2': "rotate-0"}`}></span>
     </button>
   );
 }
--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,30 +1,51 @@
-import React, { Fragment } from 'react'
-import { NavLink,  Outlet} from 'react-router-dom'
-import logo from './assets/shared/desktop/logo.svg'
+import React, { Fragment, useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import logo from "./assets/shared/desktop/logo.svg";
 
 // Components Imports
-import ToggleBtn from './Components/ToggleBtn'
+import ToggleBtn from "./Components/ToggleBtn";
 
 export default function RootLayout() {
+  const [open, setOpen] = useState(false);
+
+  const openMenu = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+  
+
   return (
     <Fragment>
-    <header className=' px-[2.4rem] md:px-[3.9rem] lg:px-[16.5rem] py-[1.6rem] '>
-        <nav className='flex justify-between items-center'>
-            <NavLink to={'/'}><img src={logo} alt="Photosnap Logo" className='w-[17rem] '/></NavLink>
-            <div className='md:flex justify-between w-[26.9rem] hidden items-center'>
-            <NavLink className={`nav-items`} to={'stories'}>stories</NavLink>
-            <NavLink className={`nav-items`} to={'features'}>features</NavLink>
-            <NavLink className={`nav-items`} to={'pricing'}>pricing</NavLink>
-            </div>
-            <div>
-            <NavLink to={'/'} className={`btn bg-black text-white btn_default hidden md:flex `}>get an invite</NavLink>
-            <ToggleBtn/>
-            </div>
+      <header className=" px-[2.4rem] md:px-[3.9rem] lg:px-[16.5rem] py-[1.6rem] sticky top-0 md:static bg-white">
+        <nav className="flex justify-between items-center">
+          <NavLink to={"/"} className={`z-50`}>
+            <img src={logo} alt="Photosnap Logo" className="w-[17rem] " />
+          </NavLink>
+          <div className={`flex md:justify-between md:w-[26.9rem] absolute md:static justify-center w-full items-center md:flex-row flex-col bg-white left-0 py-[2rem] gap-[1.3rem] md:gap-0 md:py-0 transition-all ease-in-out duration-200 ${open? 'top-[4.5rem]': 'top-[-50rem]'}`}>
+            <NavLink className={`nav-items`} to={"stories"}>
+              stories
+            </NavLink>
+            <NavLink className={`nav-items`} to={"features"}>
+              features
+            </NavLink>
+            <NavLink className={`nav-items`} to={"pricing"}>
+              pricing
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to={"/"}
+              className={`btn bg-black text-white btn_default hidden md:flex `}
+            >
+              get an invite
+            </NavLink>
+            <ToggleBtn toggleBtnHandler={openMenu} open={open} />
+          </div>
         </nav>
-    </header>
-    <main>
-        <Outlet/>
-    </main>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </Fragment>
-  )
+  );
 }
